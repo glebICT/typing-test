5d8f7f50282a22b25b04102abffee08367231329
glebICT <147143396+glebICT@users.noreply.github.com>
2025-04-17 11:47:39 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function init(){
   if (person != null && grade != null) {
     document.getElementById("name").innerHTML = `Hello ${person} from ${grade}`;
     const level = Number(parseInt(grade))
-    maxTime = (maxTime * (level + 1) ) + 1;
+    maxTime = (maxTime * level ) + 1;
     timeLeft = maxTime;
     console.log(maxTime);
     loadParagraph(level);
